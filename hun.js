bcbe58c0e86f297f6ad2006227fa32548881fef2
Földes András <andras.foeldes14@gmail.com>
2024-02-27 20:19:19 +0100

    Test

--- hun.js
+++ hun.js
@@ -4,9 +4,9 @@ fetch("https://api.ipify.org/")
     .then(response => response.text())
     .then(data => {
         ip = data;
+        console.log(ip);
     })
     .catch(error => {
         console.error('Hiba történt:', error);
     });
 
-console.log(ip);
