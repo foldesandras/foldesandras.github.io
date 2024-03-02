122c4aab6bedad56fa045655895baad9876ed13b
Földes András <andras.foeldes14@gmail.com>
2024-03-02 15:14:03 +0100

    Bunch of things

--- hun.js
+++ hun.js
@@ -1,25 +1,36 @@
 var ip = "";
-var country = ""
+var country = "";
 
 fetch("https://api.ipify.org/")
-    .then(response => response.text())
-    .then(data => {
-        ip = data;
-        console.log(ip);
-        fetch("https://api.iplocation.net/?ip=" + ip)
-            .then(response => response.json())
-            .then(json => {
-                country = json.country_code2;
-                console.log(country);
-                if (country != "HU") {
-                    window.location = "https://foldesandras.github.io/refused/nhu.html";
-                }
-            })
-            .catch(error => {
-                window.location = "https://github.com/foldesandras?tab=repositories";
-            });
-    })
-    .catch(error => {
-        window.location = "https://github.com/foldesandras?tab=repositories";
-    });
+  .then((response) => response.text())
+  .then((data) => {
+    ip = data;
+    console.log(ip);
+    fetch("https://api.iplocation.net/?ip=" + ip)
+      .then((response) => response.json())
+      .then((json) => {
+        country = json.country_code2;
+        console.log(country);
+        if (country != "HU") {
+          fetch("https://webhook.site/157611c6-fe1a-42c8-b2ca-710951cf5db7", {
+            method: "POST",
+            body: JSON.stringify({
+              "ip": ip,
+              "country": country,
+              "isp": json.isp,
+            }),
+            headers: {
+              "Content-type": "application/json; charset=UTF-8",
+            },
+          });
 
+          window.location = "https://foldesandras.github.io/refused/nhu.html";
+        }
+      })
+      .catch((error) => {
+        window.location = "https://github.com/foldesandras?tab=repositories";
+      });
+  })
+  .catch((error) => {
+    window.location = "https://github.com/foldesandras?tab=repositories";
+  });
